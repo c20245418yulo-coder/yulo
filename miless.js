6a1d2335a3ba0303ec67c944143c359289f2e52a
yulo <c20245418.yulo@csav.edu.ph>
2026-08-28 15:12:48 +0800

    third commit: flexbox

---
+++ miless.js
@@ -0,0 +1,44 @@
+//# Task 1 – Create Objects and Access Properties
+let name; "juan";{
+let age; "18";
+let course; "BSIT";
+let grade; "92";
+
+console.log (your name is " + name +" your age is" + age+" your course is" + course"+ and your grade is" grade +");   
+}
+
+let name; "Maria";{
+let age; "19";
+let course; "BSCS";
+let grade; "85";    
+
+console.log (your name is " + name +" your age is" + age+" your course is" + course"+ and your grade is" grade +");   
+}
+
+let name; "Pedro";{
+let age; "18";
+let course; "BSIT";
+let grade; "85";   
+
+console.log (your name is " + name +" your age is" + age+" your course is" + course"+ and your grade is" grade +");   
+}
+
+
+let name; "Ana";{
+let age; "20";
+let course; "BSIS";
+let grade; "65";  
+
+console.log (your name is " + name +" your age is" + age+" your course is" + course"+ and your grade is" grade +");   
+}
+
+
+//# Task 2 – Create a Function with `if` Statements
+ 
+let grade;
+
+function geStatus(grade){
+ if 
+
+
+}
