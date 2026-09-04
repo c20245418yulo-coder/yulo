c68f7a631688f2a5075a295fc21d044e9f951d2d
yulo <c20245418.yulo@csav.edu.ph>
2026-09-04 17:07:49 +0800

    .style

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ console.log(greet("mayung aga", ["Miles", "Bai", "Soy", "Tol", "Erp"]));
 
 const heading = document.querySelector("h1");
 heading.textContent = "Mansueto Yulo jr.";
+heading. style.color = "purple";
+heading. style.backgroundColor = "yellow";
+heading. style.fontSize = "100px";   
 console.log(heading);
 
 const contactHeading = document.querySelector("#contact h2");
