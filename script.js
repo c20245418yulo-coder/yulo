8954f231f3dd34a54227fab7172d290a92e44cae
yulo <c20245418.yulo@csav.edu.ph>
2026-09-04 14:38:46 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console. log("Hello, World!");
 console.log("From script.js file.");
+
+const myName = "Miles";
+let age = 20;
+age = 21;
+let myNumber = 9105794473;
+const address = "Basag 3";
+
+
+console.log(`My name is ${myName}, ${age} years old.`);
+console.log(`Number:${myNumber}.`);
+console.log(`Address: ${address}.`);
+console.log(` ${67}`);
+
+
+
